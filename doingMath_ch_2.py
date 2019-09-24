ec7498a4e129d0108edb31d2136c5babf421c56f
danzyxr <danzyxr@gmail.com>
2019-09-24 14:32:21 -0500

    Working with matplotlib, added .gitignore

--- doingMath_ch_2.py
+++ doingMath_ch_2.py
@@ -1,13 +1,38 @@
-myList = [1, 2, 3]
-for item in myList:
-    print(item)
+import matplotlib.pyplot as plt
 
-for index, item in enumerate(myList):
-    print(index, item)
+# myList = [1, 2, 3]
+# for item in myList:
+#     print(item)
 
-myTuple = (4, 5, 6)
-for item in myTuple:
-    print(item)
+# for index, item in enumerate(myList):
+#     print(index, item)
 
-for index, item in enumerate(myTuple):
-    print(index, item)
+# myTuple = (4, 5, 6)
+# for item in myTuple:
+#     print(item)
+
+# for index, item in enumerate(myTuple):
+#     print(index, item)
+
+
+def myFunction(x):
+    return (3*(x**2)) + (6*x) + 9
+
+
+x_coords = []
+y_coords = []
+
+# n = 1
+# while True:
+#     x_coords.append(n)
+#     y_coords.append(myFunction(n))
+#     if n == 10:
+#         break
+#     n = n + 1
+
+for i in range(100):
+    x_coords.append(i)
+    y_coords.append(myFunction(i))
+
+plt.plot(x_coords, y_coords, marker='o')
+plt.show()

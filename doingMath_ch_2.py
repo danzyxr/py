40c49334ad80c120bc9e3f46ef095948f6c02c28
danzyxr <danzyxr@gmail.com>
2019-09-26 14:42:13 -0500

    Plot labels, custom axes, and saved plots

--- doingMath_ch_2.py
+++ doingMath_ch_2.py
@@ -37,7 +37,12 @@ for n in range(100):
     y_coords.append(myFunction(n))
 
 plt.plot(x_coords, y_coords, marker='o')
+# plt.axis(xmin=0, xmax=100)
+# plt.axis(ymin=0, ymax=10000)
+# print(plt.axis())
+plt.axis([0, 100, 0, 10000])
 plt.show()
+# plt.savefig('myFunction.png')
 
 nyc_temp_2000 = [31.3, 37.3, 47.2, 51.0, 63.5, 71.3, 72.3, 72.7, 66.0, 57.0, 45.3, 31.1]
 nyc_temp_2006 = [40.9, 35.7, 43.1, 55.7, 63.1, 71.0, 77.9, 75.8, 66.6, 56.2, 51.9, 43.6]
@@ -46,4 +51,12 @@ nyc_temp_2012 = [37.3, 40.9, 50.9, 54.8, 65.1, 71.0, 78.8, 76.7, 68.8, 58.0, 43.
 months = range(1, 13)
 plt.plot(months, nyc_temp_2000, months, nyc_temp_2006, months, nyc_temp_2012)
 plt.legend([2000, 2006, 2012])
+plt.xlabel('Months')
+plt.ylabel('Temperature')
+plt.title('NYC\'s Average Monthly Temperature')
+# print(plt.axis())
+# plt.axis(xmin=1, xmax=12)
+# plt.axis(ymin=0, ymax=80)
+plt.axis([1, 12, 0, 80])
 plt.show()
+# plt.savefig('NYC_temp.png')
